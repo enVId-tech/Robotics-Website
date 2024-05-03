7c25f8def9e74ce9a24690df65db019d7d4abd75
Erick Tran <thelittlebotengineer@gmail.com>
2024-05-02 19:49:11 -0700

    Update 13

--- actual-robotics-web/src/assets/components/Header.tsx
+++ actual-robotics-web/src/assets/components/Header.tsx
@@ -8,6 +8,20 @@ const Header: React.FC = (): React.JSX.Element => {
         <nav id="HeaderMain">
             <span id="Header">
                 <img src={img} id="HeaderLogo" alt="Logo" />
+                <span id="ButtonContainer">
+                    <a href="/#Home" className="HeaderButton">
+                        <h1>Home</h1>
+                    </a>
+                    <a href="/#About" className="HeaderButton">
+                        <h1>About</h1>
+                    </a>
+                    <a href="/#Projects" className="HeaderButton">
+                        <h1>Projects</h1>
+                    </a>
+                    <a href="/#Contact" className="HeaderButton">
+                        <h1>Contact</h1>
+                    </a>
+                </span>
             </span>
         </nav>
     )
