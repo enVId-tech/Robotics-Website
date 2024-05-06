29c94a2b3d89a219abd53de30d56bb8c93ddb8d7
Erick Tran <thelittlebotengineer@gmail.com>
2024-05-05 19:24:10 -0700

    Update 14

--- actual-robotics-web/src/assets/components/Header.tsx
+++ actual-robotics-web/src/assets/components/Header.tsx
@@ -4,23 +4,34 @@ import '../scss/Header.scss';
 import img from '../assets/img/logo/logosmall.jpg';
 
 const Header: React.FC = (): React.JSX.Element => {
+    const [hovered, setHovered] = React.useState<boolean[]>([]);
+    const [headerTitles, setHeaderTitles] = React.useState<string[]>(['Home', 'About', 'Projects', 'Contact']);
+
+    React.useEffect(() => {
+        const newHovered: boolean[] = [];
+        headerTitles.forEach(() => newHovered.push(false));
+        setHovered(newHovered);
+    }, [headerTitles]);
+
+    const handleMouse = (index: number, value: boolean) => {
+        const newHovered = hovered.map((hover, i) => i === index ? value : hover);
+        setHovered(newHovered);
+    }
+
     return (
         <nav id="HeaderMain">
             <span id="Header">
                 <img src={img} id="HeaderLogo" alt="Logo" />
                 <span id="ButtonContainer">
-                    <a href="/#Home" className="HeaderButton">
-                        <h1>Home</h1>
-                    </a>
-                    <a href="/#About" className="HeaderButton">
-                        <h1>About</h1>
-                    </a>
-                    <a href="/#Projects" className="HeaderButton">
-                        <h1>Projects</h1>
-                    </a>
-                    <a href="/#Contact" className="HeaderButton">
-                        <h1>Contact</h1>
-                    </a>
+                    {
+                        headerTitles.map((title: string, index: number) => {
+                            return (
+                                <a href={`/#${title}`} key={index} className={hovered[index] ? "HeaderButton isHovered" : "HeaderButton"} onMouseEnter={() => handleMouse(index, true)} onMouseLeave={() => handleMouse(index, false)}>
+                                    <h1 className='TitleHeader'>{title}</h1>
+                                </a>
+                            )
+                        })
+                    }
                 </span>
             </span>
         </nav>
