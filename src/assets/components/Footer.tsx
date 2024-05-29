2bd3f16c2b33739a655a2209cd2eb520a2a9d911
Erick <thelittlebotengineer@gmail.com>
2024-05-29 19:38:57 +0700

    Update 27

--- src/assets/components/Footer.tsx
+++ src/assets/components/Footer.tsx
@@ -5,13 +5,13 @@ const Footer: React.FC = (): React.JSX.Element => {
     return (
         <section id="footer">
             <div id="left">
-                <p id="name"><a id="self-credit" href="https://github.com/enVId-tech" target="_blank" rel="noreferrer">Erick Tran</a></p>
+                <p id="name"><a id="self-credit" href="https://github.com/enVId-tech" target="_blank" rel="noopener">Erick Tran</a></p>
                 <p id="accreditation">Built for exclusive use for OA Robotics</p>
                 <p id="year">OA Robotics Website &copy; 2024</p>
             </div>
             <div id="right">
                 <p id="bugs_warning">This is an ALPHA build, bugs beware!</p>
-                <p id="update_name">Update 26 - May 29, 2024</p>
+                <p id="update_name">Update 27 - May 29, 2024</p>
             </div>
         </section>
     );
