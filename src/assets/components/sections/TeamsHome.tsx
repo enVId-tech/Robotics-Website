fe8b66fd5f29a3e86c09cc8bfac68a2297f6693c
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-26 00:51:35 -0700

    style: worked on teams portion of website

--- src/assets/components/sections/TeamsHome.tsx
+++ src/assets/components/sections/TeamsHome.tsx
@@ -4,7 +4,13 @@ import '../../scss/sections/TeamsHome.scss';
 const TeamsHome: React.FC = (): React.JSX.Element => {
     return (
         <section id="teamshome">
-
+            <div id="container">
+                <div className="team">
+                    <img src="" alt="Team" />
+                    <h1 className="title">FRC Team 4079</h1>
+                    <p className="description">Our Premiere FIRST Robotics Competition team, Quantum Leap, competes in the FRC competition.</p>
+                </div>
+            </div>
         </section>
     )
 }
