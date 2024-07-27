9b3904545990511ffc740daf86c03507013901a5
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-26 19:29:03 -0700

    style: worked on styling the teams section to make logos work

--- src/assets/components/sections/TeamsHome.tsx
+++ src/assets/components/sections/TeamsHome.tsx
@@ -1,35 +1,46 @@
 import React from "react";
 import '../../scss/sections/TeamsHome.scss';
+import FRC from '../../assets/new-img/logos/FRC.png';
+import FTC from '../../assets/new-img/logos/FTC.png';
+import VRC from '../../assets/new-img/logos/VRC.png';
+import VIQ from '../../assets/new-img/logos/VIQ.png';
 
 const TeamsHome: React.FC = (): React.JSX.Element => {
     return (
         <section id="teamshome">
             <div id="container">
                 <div className="team">
-                    <img src="" alt="Team" />
-                    <img src="" alt="FIRST Robotics Competition" />
                     <div className="team-info">
+                        <img src={FRC} alt="FIRST Robotics Competition" />
                         <h1 className="title">Team 4079</h1>
-                        <p className="description">Our Premiere FIRST Robotics Competition team, Quantum Leap, competes in the FRC competition.</p>
+                        <p className="description">Our premiere FIRST Robotics Competition team, Quantum Leap, competes in the FRC competition.</p>
                     </div>
                 </div>
                 <div className="team">
-                    <img src="" alt="Team" />
-                    <img src="" alt="FIRST Tech Challenge" />
                     <div className="team-info">
-                        <h1 className="title">FTC Team 19152A</h1>
+                        <img src={FTC} alt="FIRST Tech Challenge" />
+                        <h1 className="title">Team 19152A</h1>
                         <p className="description">Our FIRST Tech Challenge team, Quantum Leap, competes in the FTC competition.</p>
                     </div>
                     <div className="team-info">
-                        <h1 className="title">FTC Team 19152B</h1>
+                        <img src={FTC} alt="FIRST Tech Challenge" />
+                        <h1 className="title">Team 19152B</h1>
                         <p className="description">Our FIRST Tech Challenge team, Quantum Leap, competes in the FTC competition.</p>
                     </div>
                 </div>
                 <div className="team">
-                    <img src="" alt="Team" />
-                    <img src="" alt="Vex Robotics" />
-                    <h1 className="title">VRC Team 1774</h1>
-                    <p className="description">Our Vex Robotics Competition team, Quantum Leap, competes in the VRC competition.</p>
+                    <div className="team-info">
+                        <img src={VRC} alt="Vex Robotics Competition" />
+                        <h1 className="title">Team 1</h1>
+                        <p className="description">Our Vex Robotics Competition team, Quantum Leap, competes in the VRC competition.</p>
+                    </div>
+                </div>
+                <div className="team">
+                    <div className="team-info">
+                        <img src={VIQ} alt="Vex IQ" />
+                        <h1 className="title">Team 1</h1>
+                        <p className="description">Our Vex IQ team, Quantum Leap, competes in the VIQ competition.</p>
+                    </div>
                 </div>
             </div>
         </section>
