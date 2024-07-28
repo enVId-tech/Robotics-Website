f85083a95b46577aa979e542ebb180b1d7e64fc5
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-28 12:42:35 -0700

    feat: worked on main impact tsx

--- src/assets/components/sections/Impact.tsx
+++ src/assets/components/sections/Impact.tsx
@@ -1,15 +1,52 @@
 import React from "react";
 import '../../scss/sections/Impact.scss';
 
+interface Info {
+    statistic: string;
+    description: string;
+}
+
 const Impact: React.FC = (): React.JSX.Element => {
+    const info: Info[] = [
+        {
+            statistic: "52",
+            description: "Students",
+        },
+        {
+            statistic: "10",
+            description: "FRC Robotics Built",
+        },
+        {
+            statistic: "13",
+            description: "Industry Mentors",
+        },
+        {
+            statistic: "12",
+            description: "Active Alumni",
+        }
+    ]
+
     return (
         <section id="impact">
             <div id="container">
                 <div id="left">
-                
+                    <div id="left-top">
+                        <h1 className="title">Our Impact</h1>
+                        <p className="description">Our team raises awareness about STEM, as the only FRC Team in the Anaheim Union High School District.</p>
+                    </div>
+                    <div id="left-bottom">
+                        {info.map((data: Info, index: number) => {
+                            return (
+                                <div key={index} className="info">
+                                    <h1 className="statistic">{data.statistic}</h1>
+                                    <p className="description">{data.description}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
                 <div id="right">
-                    
+
                 </div>
             </div>
         </section>
--- src/assets/components/sections/TeamsHome.tsx
+++ src/assets/components/sections/TeamsHome.tsx
@@ -5,48 +5,67 @@ import FTC from '../../assets/new-img/logos/FTC.png';
 import VRC from '../../assets/new-img/logos/VRC.png';
 import VIQ from '../../assets/new-img/logos/VIQ.png';
 
-interface Descriptor {
-    title: string;
+interface Team {
     team: string;
     description: string;
     image?: string;
 }
 
+interface Descriptor<T = Team[]> {
+    title: string;
+    teams: T;
+}
+
 const TeamsHome: React.FC = (): React.JSX.Element => {
     const [activeTeam, setActiveTeam] = React.useState<string>('FRC');
 
-    const descriptors: Descriptor[] = [
+    const descriptorsTemp: Descriptor[] = [
         {
             title: "FRC",
-            team: "4079",
-            description: "Our premiere FIRST Robotics Competition team, Quantum Leap, competes in the FRC competition.",
-            image: FRC,
+            teams: [
+                {
+                    team: "4079",
+                    description: "Our premiere FIRST Robotics Competition team, Quantum Leap, competes in the FRC competition.",
+                    image: FRC,
+                }
+            ]
         },
         {
             title: "FTC",
-            team: "19152A",
-            description: "Our FIRST Tech Challenge team, Quantum Leap, competes in the FTC competition.",
-            image: FTC,
-        },
-        {
-            title: "FTC",
-            team: "19152B",
-            description: "Our FIRST Tech Challenge team, Quantum Leap, competes in the FTC competition.",
-            image: FTC,
+            teams: [
+                {
+                    team: "19152A",
+                    description: "Our FIRST Tech Challenge team, Quantum Leap, competes in the FTC competition.",
+                    image: FTC,
+                },
+                {
+                    team: "19152B",
+                    description: "Our FIRST Tech Challenge team, Quantum Leap, competes in the FTC competition.",
+                    image: FTC,
+                }
+            ]
         },
         {
             title: "VRC",
-            team: "1",
-            description: "Our Vex Robotics Competition team, Quantum Leap, competes in the VRC competition.",
-            image: VRC,
+            teams: [
+                {
+                    team: "1",
+                    description: "Our Vex Robotics Competition team, Quantum Leap, competes in the VRC competition.",
+                    image: VRC,
+                }
+            ]
         },
         {
             title: "VIQ",
-            team: "1",
-            description: "Our Vex IQ team, Quantum Leap, competes in the VIQ competition.",
-            image: VIQ,
+            teams: [
+                {
+                    team: "1",
+                    description: "Our Vex IQ team, Quantum Leap, competes in the VIQ competition.",
+                    image: VIQ,
+                }
+            ]
         }
-    ];
+    ]
 
     React.useEffect(() => {
         const handleScroll = () => {
@@ -82,13 +101,21 @@ const TeamsHome: React.FC = (): React.JSX.Element => {
         <section id="teamshome">
             <div id="container">
                 {
-                    descriptors.map((descriptor: Descriptor, index: number): React.JSX.Element => {
+                    descriptorsTemp.map((descriptor: Descriptor, index: number): React.JSX.Element => {
                         return (
-                            <div key={index} className="team" id={descriptor.title}>
+                            <div key={index} className={`team ${activeTeam === descriptor.title ? 'active' : ''}`} id={descriptor.title}>
                                 <div className="team-info">
-                                    <img src={descriptor.image} alt="Team" />
-                                    <h1 className="title">Team {descriptor.team}</h1>
-                                    <p className="description">{descriptor.description}</p>
+                                    {
+                                        descriptor.teams.map((team: Team, index: number): React.JSX.Element => {
+                                            return (
+                                                <div key={index} className={`team-info-desc ${activeTeam === descriptor.title ? 'active' : ''}`}>
+                                                    <img src={team.image} alt="Team" />
+                                                    <h1 className="title">Team {team.team}</h1>
+                                                    <p className="description">{team.description}</p>
+                                                </div>
+                                            )
+                                        })
+                                    }
                                 </div>
                             </div>
                         )
