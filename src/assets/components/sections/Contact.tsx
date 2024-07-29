e55e68cb953553f73c7c3ceb8969526fe7cd8ecd
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-29 11:55:51 -0700

    feat: finished contact section

--- src/assets/components/sections/Contact.tsx
+++ src/assets/components/sections/Contact.tsx
@@ -6,8 +6,18 @@ const Contact: React.FC = (): React.JSX.Element => {
         <section id="contact">
             <div id="container">
                 <h1 id="title">
-                    Contact Us
+                    Get In Touch
                 </h1>
+                <h3 id="subtitle">
+                    Send us a message or check out our socials!
+                </h3>
+                <div id="contact-info">
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="text" placeholder="Subject" />
+                </div>
+                <textarea placeholder="Message" rows={10} cols={50} />
+                <button>Submit Message</button>
             </div>
         </section>
     )
